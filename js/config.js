cd7d293ef23de9721bbe2bdc0a0390bc95754c26
Futuret3chdev <jason.c@futuret3ch.com.au>
2026-06-19 23:10:57 +1000

    Updated Discord client ID v20

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@ const MTEPOP_CONFIG = {
   facebookAppId: '',
   xClientId: '',
   xClientSecret: '',
-  discordClientId: '1366933578214277140',
+  discordClientId: '1348440616442265641',
   // Public bot username only (no @). Safe to commit.
   telegramBotUsername: 'mod_futuret3ch_bot',
   // 'deeplink' = shared multi-game bot. 'widget' = Login Widget (needs exclusive /setdomain).
