b87b541179fda36856e703929083d0268671fe3d
Futuret3chdev <jason.c@futuret3ch.com.au>
2026-06-19 21:13:47 +1000

    Revert menu to classic layout, keep Discord/Telegram v13

--- js/pwa.js
+++ js/pwa.js
@@ -1,7 +1,7 @@
 const PWA = (() => {
   function init() {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js?v=12').catch(() => {});
+      navigator.serviceWorker.register('/sw.js?v=13').catch(() => {});
     }
   }
 
